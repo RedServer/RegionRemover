6d2ca7132e92254beabf742cf29959f273724067
TheAndrey <andrey9506@yandex.ru>
2016-12-11 15:53:08 +0200

    Прерывание по тайм-ауту

--- src/theandrey/regionremover/RemoveTask.java
+++ src/theandrey/regionremover/RemoveTask.java
@@ -3,35 +3,35 @@ package theandrey.regionremover;
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.managers.storage.StorageException;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.scheduler.BukkitTask;
 
 public final class RemoveTask implements Runnable {
 
-	public static final int LIMIT = 100;
-
 	private final static Set<String> adminRegions = new HashSet<>();
 	private final WorldGuardPlugin wg;
 	private final List<RegionManager> manages = new ArrayList<>();
 	private final BukkitTask task;
+
+	static {
+		adminRegions.add("__global__");
+		adminRegions.add("spawn");
+		adminRegions.add("spawn-area");
+		adminRegions.add("spawn_area");
+	}
 
 	public RemoveTask(RegionRemoverPlugin plugin) {
 		wg = WGBukkit.getPlugin();
 
 		for(World world : Bukkit.getWorlds()) {
 			RegionManager mgr = wg.getRegionManager(world);
-			if(mgr == null) {
-				RegionRemoverPlugin.log.info("Пропускаем мир " + world.getName());
-				continue;
-			}
+			if(mgr == null) continue;
 			manages.add(mgr);
 		}
 
@@ -48,35 +48,23 @@ public final class RemoveTask implements Runnable {
 
 		RegionManager mgr = manages.get(0);
 
-		int count = 0;
 		boolean stopByLimit = false;
+		final long startTime = System.currentTimeMillis();
 
 		for(ProtectedRegion region : new ArrayList<>(mgr.getRegions().values())) {
 			String id = region.getId();
 			if(adminRegions.contains(id.toLowerCase())) continue;
 			mgr.removeRegion(id);
 			RegionRemoverPlugin.log.info("Удалён регион: " + id);
-			count++;
-			if(count >= LIMIT) {
+
+			// Прерывание по тайм-ауту
+			if((System.currentTimeMillis() - startTime) >= 1000) {
 				stopByLimit = true;
 				break;
 			}
 		}
 
-		try {
-			mgr.save();
-		} catch (StorageException ex) {
-			RegionRemoverPlugin.log.log(Level.SEVERE, "Save fail", ex);
-		}
-
 		if(!stopByLimit) manages.remove(mgr);
-	}
-
-	static {
-		adminRegions.add("__global__");
-		adminRegions.add("spawn");
-		adminRegions.add("spawn-area");
-		adminRegions.add("spawn_area");
 	}
 
 }
