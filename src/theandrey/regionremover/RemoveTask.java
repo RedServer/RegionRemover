20711a390f208b8b3aa8e48b3475408bf61d0acf
TheAndrey <andrey9506@yandex.ru>
2016-12-16 18:57:11 +0200

    Используем список миров

--- src/theandrey/regionremover/RemoveTask.java
+++ src/theandrey/regionremover/RemoveTask.java
@@ -3,6 +3,7 @@ package theandrey.regionremover;
 import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.GlobalProtectedRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -16,55 +17,58 @@ public final class RemoveTask implements Runnable {
 
 	private final static Set<String> adminRegions = new HashSet<>();
 	private final WorldGuardPlugin wg;
-	private final List<RegionManager> manages = new ArrayList<>();
+	private final List<World> worlds = new ArrayList<>();
 	private final BukkitTask task;
+	private final String defaultWorld;
 
 	static {
-		adminRegions.add("__global__");
 		adminRegions.add("spawn");
 		adminRegions.add("spawn-area");
-		adminRegions.add("spawn_area");
+		adminRegions.add("road-north");
+		adminRegions.add("road-south");
+		adminRegions.add("road-west");
+		adminRegions.add("road-east");
 	}
 
 	public RemoveTask(RegionRemoverPlugin plugin) {
 		wg = WGBukkit.getPlugin();
 
-		for(World world : Bukkit.getWorlds()) {
-			RegionManager mgr = wg.getRegionManager(world);
-			if(mgr == null) continue;
-			manages.add(mgr);
-		}
+		worlds.addAll(Bukkit.getWorlds());
+		defaultWorld = Bukkit.getWorlds().get(0).getName();
 
 		task = Bukkit.getScheduler().runTaskTimer(plugin, this, 0, 1);
 	}
 
 	@Override
 	public void run() {
-		if(manages.isEmpty()) {
+		if(worlds.isEmpty()) {
 			RegionRemoverPlugin.log.info("Удаление регионов завершено.");
 			task.cancel();
 			return;
 		}
 
-		RegionManager mgr = manages.get(0);
-
 		boolean stopByLimit = false;
-		final long startTime = System.currentTimeMillis();
+		World world = worlds.get(0);
+		RegionManager mgr = wg.getRegionManager(world);
 
-		for(ProtectedRegion region : new ArrayList<>(mgr.getRegions().values())) {
-			String id = region.getId();
-			if(adminRegions.contains(id.toLowerCase())) continue;
-			mgr.removeRegion(id);
-			RegionRemoverPlugin.log.info("Удалён регион: " + id);
+		if(mgr != null) {
+			final long startTime = System.currentTimeMillis();
+			for(ProtectedRegion region : new ArrayList<>(mgr.getRegions().values())) {
+				if(region instanceof GlobalProtectedRegion) continue; // нельзя удалять
+				String id = region.getId();
+				if(world.getName().equalsIgnoreCase(defaultWorld) && adminRegions.contains(id.toLowerCase())) continue;
+				mgr.removeRegion(id);
+				RegionRemoverPlugin.log.info("Удалён регион: " + id);
 
-			// Прерывание по тайм-ауту
-			if((System.currentTimeMillis() - startTime) >= 1000) {
-				stopByLimit = true;
-				break;
+				// Прерывание по тайм-ауту
+				if((System.currentTimeMillis() - startTime) >= 1000) {
+					stopByLimit = true;
+					break;
+				}
 			}
 		}
 
-		if(!stopByLimit) manages.remove(mgr);
+		if(!stopByLimit) worlds.remove(world);
 	}
 
 }
