7228a54e88a6f25ade84b889b43bc2ca1d201145
TheAndrey <andrey9506@yandex.ru>
2017-06-02 00:05:40 +0200

    Вывод сообщения о завершении удаления

--- src/main/java/theandrey/regionremover/RemoveTask.java
+++ src/main/java/theandrey/regionremover/RemoveTask.java
@@ -5,21 +5,26 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.GlobalProtectedRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitTask;
 
 public final class RemoveTask implements Runnable {
 
 	private final static Set<String> adminRegions = new HashSet<>();
+
 	private final WorldGuardPlugin wg;
 	private final List<World> worlds = new ArrayList<>();
-	private final BukkitTask task;
+	private BukkitTask task;
 	private final String defaultWorld;
+	private final WeakReference<CommandSender> launchedBy;
 
 	static {
 		adminRegions.add("spawn");
@@ -30,20 +35,26 @@ public final class RemoveTask implements Runnable {
 		adminRegions.add("road-east");
 	}
 
-	public RemoveTask(RegionRemoverPlugin plugin) {
+	public RemoveTask(CommandSender launchedBy) {
 		wg = WGBukkit.getPlugin();
+		this.launchedBy = new WeakReference<>(launchedBy);
 
 		worlds.addAll(Bukkit.getWorlds());
 		defaultWorld = Bukkit.getWorlds().get(0).getName();
+	}
 
+	public void start(RegionRemoverPlugin plugin) {
 		task = Bukkit.getScheduler().runTaskTimer(plugin, this, 0, 1);
 	}
 
 	@Override
 	public void run() {
 		if(worlds.isEmpty()) {
-			RegionRemoverPlugin.log.info("Удаление регионов завершено.");
 			task.cancel();
+
+			CommandSender sender = launchedBy.get();
+			if(sender != null) sender.sendMessage(ChatColor.GREEN + "Удаление регионов завершено.");
+			RegionRemoverPlugin.log.info("Удаление регионов завершено.");
 			return;
 		}
 
